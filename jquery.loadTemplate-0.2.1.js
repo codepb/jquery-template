b0df24eb4f637e4d9438af8ba10c6650c7f17518
Paul Burgess <paul.j.burgess@outlook.com>
2013-05-28 21:44:54 +0100

    Added support for iterating over an array
    Accepts an array of objects as well as a single object. If an array is
    provided for the data, the function iterates over the array and
    populates the template for each. Fixed some underlying bugs

--- jquery.loadTemplate-0.2.1.js
+++ jquery.loadTemplate-0.2.1.js
@@ -11,6 +11,32 @@
 
     function loadTemplate(template, data, options) {
         var $that = this;
+        if($.type(data) === "array") {
+            var todo = data.length;
+            var done = 0;
+            options = options || {};
+            var newOptions = $.extend(
+                {},
+                options,
+                {
+                    complete: function() {
+                        $that.append(this);
+                        done++;
+                        if(done == todo) {
+                            if(options && typeof options.complete == "function") {
+                                options.complete();
+                            }
+                        }
+                    }
+                }
+            );
+            $(data).each(function(){
+                var $div = $("<div/>");
+                
+                loadTemplate.call($div, template, this, newOptions);
+            });
+            return this;
+        }
         var settings = $.extend({
             // These are the defaults.
             overwriteCache: false,
@@ -36,15 +62,15 @@
 
         if (isFile && !settings.overwriteCache && templates[template]) {
             $templateContainer = templates[template];
-            prepareTemplate.call($that, $templateContainer, data);
+            prepareTemplate.call($that, $templateContainer, data, settings.complete);
             if (typeof settings.success == "function") {
                 settings.success();
             }
         } else if (isFile && !settings.overwriteCache && templates.hasOwnProperty(template)) {
             if (queue[template]) {
-                queue[template].push(data);
+                queue[template].push({ data: data, selection: $that, settings: settings});
             } else {
-                queue[template] = [{ data: data, selection: $that}];
+                queue[template] = [{ data: data, selection: $that, settings: settings}];
             }
         } else if (isFile) {
             var $templateContainer = $("<div/>");
@@ -53,31 +79,36 @@
                 if (textStatus == "error") {
                     if (typeof settings.error == "function") {
                         settings.error.call($that);
-                        $(queue[template]).each(function (key, value) {
-                            settings.error.call(value.selection);
-                        });
                     }
+                    $(queue[template]).each(function (key, value) {
+                        if(typeof value.settings.error == "function") {
+                            value.settings.error.call(value.selection);
+                        }
+                    });
                     if (typeof settings.complete === "function") {
                         settings.complete.call($that);
                         var value;
-                        while(value = queue[template].pop()) {
-                            settings.complete.call(value.selection);
+                    }
+                    while(value = queue[template].shift()) {
+                        if(typeof value.settings.complete === "function") {
+                            value.settings.complete.call(value.selection);
                         }
                     }
                     if(queue[template].length > 0) {
                         queue[template] = [];
                     }
                 } else {
-                    templates[template] = $templateContainer;
-                    prepareTemplate.call($that, $templateContainer, data);
+
+                    templates[template] = $templateContainer.clone();
+                    prepareTemplate.call($that, $templateContainer, data, settings.complete);
                     if (textStatus == "success" && typeof settings.success == "function") {
                         settings.success.call($that);
                     }
                     var value;
-                    while(value = queue[template].pop()) {
-                        prepareTemplate.call(value.selection, $templateContainer, value.data)
-                        if (textStatus == "success" && typeof settings.success == "function") {
-                            settings.success.call(value.selection);
+                    while(value = queue[template].shift()) {
+                        prepareTemplate.call(value.selection, templates[template].clone(), value.data, value.settings.complete)
+                        if (textStatus == "success" && typeof value.settings.success == "function") {
+                            value.settings.success.call(value.selection);
                         }
                     }
                 }
@@ -88,20 +119,20 @@
                 $template = $.parseHTML($.trim($template.html()));
             }
             $templateContainer.html($template);
-            prepareTemplate.call($that, $templateContainer, data);
+            prepareTemplate.call($that, $templateContainer, data, settings.complete);
             if (typeof settings.success == "function") {
                 settings.success();
             }
         }
         return this;
 
-        function prepareTemplate(template, data) {
+        function prepareTemplate(template, data, complete) {
             bindData(template, data);
             $(this).each(function () {
                 $(this).html(template.html());
             });
-            if (typeof settings.complete === "function") {
-                settings.complete();
+            if (typeof complete === "function") {
+                complete.call($(this));
             }
         }
 
